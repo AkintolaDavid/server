79dad157ed16894fd1ce71d7c1c8478465610824
AkintolaDavid <akintoladvaid66@gmail.com>
2024-07-06 04:14:51 +0100

    rrr

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 // app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // app.get("*", (req, res) => {
